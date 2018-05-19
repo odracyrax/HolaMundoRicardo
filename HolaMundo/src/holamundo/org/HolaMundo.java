ecb4ef262d584e32aecca384145f8a8455b5740a
Ricardo <Ricardo@DESKTOP-3EQG9AJ>
2018-05-19 16:42:45 +0200

    Segundo commit en el comentaio

--- HolaMundo/src/holamundo/org/HolaMundo.java
+++ HolaMundo/src/holamundo/org/HolaMundo.java
@@ -4,6 +4,7 @@ public class HolaMundo {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		//Segundo commit desde otro cliente
 		System.out.println("Hola Mundo");
 	}
 
